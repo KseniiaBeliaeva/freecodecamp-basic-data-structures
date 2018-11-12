0ea1d0c76c0936191ffb3c1ef5c8be620edb78a6
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-12 20:20:54 +1100

    Arrays

--- complexarrays.js
+++ complexarrays.js
@@ -1,9 +1,9 @@
 // Basic Data Structures: Create complex multi - dimensional arrays
 
 
-/* We have defined a variable, myNestedArray, set equal to an array.Modify myNestedArray, using any combination
+/* We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination
 of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth(remember,
-the outer - most array is level 1).Somewhere on the third level, include the string 'deep',
+the outer - most array is level 1). Somewhere on the third level, include the string 'deep',
 on the fourth level, include the string 'deeper', and on the fifth level, include the string 'deepest'. */
 
 
